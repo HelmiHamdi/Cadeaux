cfda222ab81cf6ac1525feb4d8386ed9525f20d1
Hamdi Helmi <helmihamdi977@gmail.com>
2025-10-31 15:01:08 +0100

    finale

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://gifit-game-app.onrender.com/api';
+const API_BASE_URL = 'https://game-gifit-app.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
