29f9af8075e32abef47f69b3518105b09a346d33
Hamdi Helmi <helmihamdi977@gmail.com>
2025-10-31 16:00:29 +0100

    updateing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL:  import.meta.env.NODE_ENV=== "development" ? "http://localhost:5001/api" : "/api",
   headers: {
     'Content-Type': 'application/json', 
   },
