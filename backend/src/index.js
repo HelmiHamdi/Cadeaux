5284e9a5b8730b452ba2aaebbaa9989ba75577d2
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-02 12:03:29 +0100

    fin of project

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,11 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", // Autorise toutes les origines
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 // --- Routes API ---
