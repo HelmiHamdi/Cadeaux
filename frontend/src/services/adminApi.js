0385423ab03b6bbc495d64407e78c1ead6e50687
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-02 08:58:40 +0100

    okitotos

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL =  'http://localhost:5000/api';
+const API_BASE_URL =  'http://192.168.84.136:5000/api';
 
 // Créer instance axios
 const adminApi = axios.create({
