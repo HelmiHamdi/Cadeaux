31aff5955f4f32c354d124d20bd76c3bdde01d34
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-04 08:45:27 +0100

    ok

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://bambou-e8x7.onrender.com/api';
 
 // Créer instance axios
 const adminApi = axios.create({
