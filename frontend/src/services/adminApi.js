7469841a9a5918c2a60234b0098f5daea3a9aed3
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-05 09:31:13 +0100

    ok

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-//const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://localhost:5000/api";
 
 // Créer instance axios
 const adminApi = axios.create({
-  baseURL: `${import.meta.env.BACKEND_URL}/api`,
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-//const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 
 
 const api = axios.create({
-  baseURL:  `${import.meta.env.BACKEND_URL}/api`,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json', 
   },
