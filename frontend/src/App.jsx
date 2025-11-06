d075e13a3dece0bb0842b518df928302cfff5298
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-06 12:53:35 +0100

    okitourffd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,42 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Admin from "./pages/Admin";
+/*import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+import About from './pages/About';
 
 function App() {
   return (
-    <Router>
-      <MainApp />
-    </Router>
+    <div className="App font-main">
+      <Header />
+      <Home />
+      <About/>
+    </div>
   );
 }
 
-// ✅ On sépare pour pouvoir utiliser useLocation à l'intérieur du Router
-function MainApp() {
-  const location = useLocation();
-  const isAdminPage = location.pathname.startsWith("/admin");
+export default App;*/
+import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Admin from './pages/Admin';
 
+function App() {
+  // Vérifier si nous sommes sur la route admin
+  const isAdminPage = window.location.pathname === '/admin';
+  
   return (
     <div className="App font-main">
-      {/* Header visible sauf sur les pages admin */}
+      {/* Ne pas afficher le Header sur la page admin */}
       {!isAdminPage && <Header />}
-
-      <Routes>
-        {/* Routes publiques */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-
-        {/* Route admin */}
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      
+      {/* Afficher le composant correspondant à la route */}
+      {isAdminPage ? <Admin /> : (
+        <>
+          <Home />
+          <About />
+        </>
+      )}
     </div>
   );
 }
