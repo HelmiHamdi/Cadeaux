1119a6a41e67336f168854c138cba0d40bfdfafb
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-06 13:17:32 +0100

    jgld

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
+
 import "./cron/weeklyDraw.js";
 import { connectDB } from "./lib/db.js";
 import giftRoutes from "./routes/giftRoutes.js";
@@ -17,16 +18,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ Obtenir le bon __dirname avec ES Modules
+
 const __dirname = path.resolve();
 
-// ✅ CORS global
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: "*", // Autorise toutes les origines
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
-
 app.use(express.json());
 
 // --- Routes API ---
@@ -35,27 +34,26 @@ app.use("/api/draws", weeklyDrawRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/codes", codeRoutes);
-app.use("/api/winners", winnerRoutes);
+app.use("/api/winners", winnerRoutes); 
+// --- Servir React en production ---
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// --- Route test API ---
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
+
+
+
+
+// --- Route racine test API ---
 app.get("/api", (req, res) => {
   res.send("🎁 Gift Game API is running!");
 });
 
-// --- Servir le frontend en production ---
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-
-  // 🟢 1. Servir les fichiers statiques
-  app.use(express.static(frontendPath));
-
-  // 🟢 2. Rediriger TOUTES les autres routes vers index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
-
-// --- Démarrer le serveur ---
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   connectDB();
