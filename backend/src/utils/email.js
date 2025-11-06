2765ed78a8614df154fef30b962514bd2865083c
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-06 15:36:48 +0100

    fdadffg

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -1,26 +1,23 @@
-import { Resend } from "resend";
-import dotenv from "dotenv";
+import sgMail from "@sendgrid/mail";
 
-dotenv.config(); // ⚡ Charger les variables
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async (to, subject, htmlContent) => {
-  try {
-    const response = await resend.emails.send({
-      from: process.env.FROM_EMAIL,
-      to,
-      subject,
-      html: htmlContent,
-    });
+  const msg = {
+    to,
+    from: process.env.FROM_EMAIL,
+    subject,
+    html: htmlContent,
+  };
 
+  try {
+    await sgMail.send(msg);
     console.log(`✅ Email envoyé à ${to}`);
-    console.log(response.data); // Pour debug
   } catch (error) {
-    console.error("❌ Erreur Resend:", error.message);
+    console.error("❌ Erreur SendGrid:", error.message);
     if (error.response) {
-      console.error(error.response);
+      console.error(error.response.body);
     }
-    throw new Error("Erreur lors de l’envoi de l’e-mail");
+    throw new Error("Erreur lors de l’envoi de l’email");
   }
 };
