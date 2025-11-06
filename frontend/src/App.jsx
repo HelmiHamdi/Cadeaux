ddf4cb67c23b06d12071a5738c04557a786b3337
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-06 12:55:57 +0100

    fin project svp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,4 @@
-/*import React from 'react';
-import Header from './components/Header';
-import Home from './pages/Home';
 
-import About from './pages/About';
-
-function App() {
-  return (
-    <div className="App font-main">
-      <Header />
-      <Home />
-      <About/>
-    </div>
-  );
-}
-
-export default App;*/
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
