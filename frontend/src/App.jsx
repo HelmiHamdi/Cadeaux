c01f8f49b8d2b94593daf2fc85f98ce4238285b9
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-08 18:58:23 +0100

    finino

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Admin from './pages/Admin';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App font-main">
-        <Switch>
-          {/* Route pour la page d'accueil */}
-          <Route exact path="/" component={() => (
-            <>
-              <Header />
-              <Home />
-              <About />
-            </>
-          )} />
-          
-          {/* Route pour la page Admin */}
-          <Route path="/admin" component={Admin} />
-        </Switch>
+        <Routes>
+          {/* Page d’accueil (home + about) */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+                <About />
+              </>
+            }
+          />
+
+          {/* Page Admin */}
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
       </div>
     </Router>
   );
