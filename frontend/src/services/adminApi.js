6fcb199f60586da2e7ec95988819b648836d39ca
Hamdi Helmi <helmihamdi977@gmail.com>
2025-11-08 18:39:47 +0100

    okito

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://cadeaux-production.up.railway.app/api";
 
 // Créer instance axios
 const adminApi = axios.create({
